0a99d45e8407dc0d1f61708bcdcfd47d6565dddf
Lazaro2 <63370459+Lazaro2@users.noreply.github.com>
2020-06-10 20:22:19 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,3 @@
 <?php
-     $servidor = "mysql669.umbler.com:41890";
-     $usuario = "alunos";
-     $senha = "";
-     $banco = "escolae";
-     $conn = new mysqli_connect($servidor, $usuario, $senha, $banco);
+$mysqli = new mysqli("mysql669.umbler.com:41890", "usuario", "senha", "database");
 ?>
